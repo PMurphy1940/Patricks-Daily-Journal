f841a9b49e0ae6360900008f17317f9f07a73ef5
Patrick Murphy <engine_ear@mac.com>
2020-05-31 17:23:36 -0500

    Added journal.js script

---
+++ src/scripts/journal.js
@@ -0,0 +1,13 @@
+const journalEntry = {
+    journalDate: "",
+    conceptsCovered: "",
+    journalEntry: "",
+    moodForTheDay: ""
+}
+
+const patrickMJournal = []
+
+patrickMJournal.push(journalEntry)
+
+console.log(patrickMJournal)
+
